d79bcc358a2bb5286635594720409bd236e365d4
João Pedro Scharf <jpscharf@hotmail.com>
2025-06-22 19:48:14 -0300

    Primeiro commit
    Primeiro commit

---
+++ sever/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    dbURI: 'mongodb+srv://joaoscharf:v1t43xpr322@original.ykt5qyu.mongodb.net/?retryWrites=true&w=majority&appName=Original',
+    jwtSecret: 'seu-segredo-jwt',
+};
