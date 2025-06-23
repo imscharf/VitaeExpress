8aa463892a454e031515bcdd9e775596f8955abc
João Pedro Scharf <jpscharf@hotmail.com>
2025-06-22 22:35:10 -0300

    Update config

--- sever/config.js
+++ sever/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    dbURI: 'mongodb+srv://joaoscharf:v1t43xpr322@original.ykt5qyu.mongodb.net/?retryWrites=true&w=majority&appName=Original',
-    jwtSecret: 'seu-segredo-jwt',
+    dbURI: process.env.DB_URI || 'mongodb+srv://joaoscharf:v1t43xpr322@original.ykt5qyu.mongodb.net/?retryWrites=true&w=majority&appName=Original',
+    jwtSecret: process.env.JWT_SECRET || 'seu-segredo-jwt',
 };
